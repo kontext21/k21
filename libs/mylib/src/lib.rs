5b63b926834764f87412cd07d3379f78d83f0be6
fzubero <fernando.zubero@empliq.com>
2025-03-06 14:17:11 +0100

    added lib ping

--- libs/mylib/src/lib.rs
+++ libs/mylib/src/lib.rs
@@ -2,7 +2,7 @@ pub mod image_sc;
 pub mod mp4_pr;
 pub mod ocr;
 pub mod logger;
-
+pub mod signal;
 // Add any other public functions you want to expose
 pub fn my_function() {
     println!("Hello from mylib!");
